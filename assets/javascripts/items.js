a4cd251b37670fc4b9eaebff1592a3d2ccbdfd86
marahin <me@marahin.pl>
2019-04-30 17:33:06 +0200

    add combination matcher

--- assets/javascripts/items.js
+++ assets/javascripts/items.js
@@ -18,6 +18,8 @@ var lastItem = function() {
   return JSON.parse(stored);
 }();
 
+var sets = [];
+
 function prepareItem(slot, name, traitOne, traitTwo) {
   const id = lastItem === null ? 0 : lastItem.id+1;
   return {
@@ -58,6 +60,87 @@ function deleteItemId(id) {
   populateTables();
 }
 
+function onlyUnique(value, index, self) {
+  return self.indexOf(value) === index;
+}
+
+function refreshCombinationCheckerOptions(){
+  const traitOnes = $.map(items, function(item) {
+    return item.traitOne;
+  })
+  const traitTwos= $.map(items, function(item) {
+    return item.traitOne;
+  })
+
+  const traits = [...traitOnes, ...traitTwos].filter(onlyUnique);
+
+  $.each(traits, function(_, trait) {
+    $('#checkerTraitOne').append(`
+      <option>${trait}</option>    
+    `);
+    $('#checkerTraitTwo').append(`
+      <option>${trait}</option>    
+    `);
+  });
+}
+
+function checkCombinationOf(traits) {
+  console.log("Traits to check: ");
+  console.log(traits);
+
+  generateSets();
+
+  var matchingSets = sets.filter(function(set){
+    var passing = true;
+
+    $.each(traits, function(trait, amount) {
+      passing = (set.traits[trait] >= amount);
+      console.log(`(set[${trait}] = ${set.traits[trait]}) >= ${amount} (amount)  [[ ${passing} ]]`);
+
+      if(!passing) { return false; }
+    })
+
+    return passing;
+  })
+
+  return matchingSets;
+}
+
+function generateSets() {
+  console.log('Generating sets...');
+
+  sets = [];
+
+  const heads = items.filter(function(item) { return item.slot === 'head'; });
+  const shoulders = items.filter(function(item) { return item.slot === 'shoulder'; });
+  const chests = items.filter(function(item) { return item.slot === 'chest'; });
+
+  console.log('items to match with sets');
+  console.log(items);
+
+  $.each(heads, function(_, head) {
+    $.each(shoulders, function(_, shoulder) {
+      $.each(chests, function(_, chest) {
+        const traits = [head.traitOne, head.traitTwo, shoulder.traitOne, shoulder.traitTwo, chest.traitOne, chest.traitTwo];
+
+        const traitSet = {};
+
+        $.each(traits, function(_, trait) {
+          traitSet[trait] = traitSet[trait] || 0;
+          traitSet[trait]++;
+        });
+
+        sets.push({
+          items: [head, shoulder, chest],
+          traits: traitSet
+        });
+      })
+    })
+  });
+  console.log(sets);
+  refreshCombinationCheckerOptions();
+}
+
 function populateTables() {
   $('#chests-tbody').empty();
   $('#heads-tbody').empty();
@@ -74,6 +157,8 @@ function populateTables() {
       </tr>
     `);
   });
+
+  generateSets();
 }
 
 $(document).ready(function(){
@@ -92,4 +177,32 @@ $('#itemForm').on('submit', function(event) {
   console.log(`Adding item. Slot: ${slot}, name: ${name}, traitOne: ${traitOne}, traitTwo: ${traitTwo}`);
 
   addItem(prepareItem(slot, name, traitOne, traitTwo));
-})
+});
+
+$('#checkerForm').on('submit', function(event) {
+  $('#checkerResult').text('Result: ...');
+  event.preventDefault();
+
+  var traits = {};
+
+  traits[$('#checkerTraitOne').val()] = parseInt($('#traitOneNo').val());
+  traits[$('#checkerTraitTwo').val()] = parseInt($('#traitTwoNo').val());
+
+  var sets = checkCombinationOf(traits);
+
+  if(sets.length > 0) {
+    $('#checkerResult').text('Result: OK!');
+
+    $.each(sets, function(_, set) {
+      itenz = $.map(set.items, function(item) {
+        return `${item.name} (${item.traitOne}, ${item.traitTwo})`;
+      });
+
+      $('#checkerResult').append(`
+        <p>Wear ${itenz.join(', ')}</p>
+      `);
+    });
+  } else {
+    $('#checkerResult').text('Result: :( No matches');
+  }
+});
